a772b3217302cd87967aeed8fd4df1e56d32a7a6
Jett Huang <hjthamakbar@gmail.com>
2023-03-03 11:37:25 +0800

    Init

---
+++ src/router/routes/modules/dashboard.ts
@@ -0,0 +1,47 @@
+import type { AppRouteModule } from '/@/router/types'
+
+import { LAYOUT } from '/@/router/constant'
+import { t } from '/@/hooks/web/useI18n'
+
+const dashboard: AppRouteModule = {
+  path: '/dashboard',
+  name: 'Dashboard',
+  component: LAYOUT,
+  redirect: '/dashboard/analysis',
+  meta: {
+    orderNo: 10,
+    icon: 'ion:grid-outline',
+    title: t('routes.dashboard.dashboard'),
+  },
+  children: [
+    {
+      path: 'analysis',
+      name: 'Analysis',
+      component: () => import('/@/views/dashboard/analysis/index.vue'),
+      meta: {
+        // affix: true,
+        title: t('routes.dashboard.analysis'),
+      },
+    },
+    {
+      path: 'workbench',
+      name: 'Workbench',
+      component: () => import('/@/views/dashboard/workbench/index.vue'),
+      meta: {
+        title: t('routes.dashboard.workbench'),
+      },
+    },
+    {
+      path: 'basicSearch',
+      name: 'BasicSearch',
+      component: () => import('/@/views/dashboard/basic-search/index.vue'),
+      meta: {
+        // affix: true,
+        frameSrc: 'http://124.222.237.67:8084/',
+        title: t('routes.dashboard.basicSearch'),
+      },
+    },
+  ],
+}
+
+export default dashboard
---
+++ src/views/dashboard/basic-search/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <iframe
+      src="http://124.222.237.67:8084/"
+      id="mobsf"
+      frameborder="0"
+      style="position: absolute"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    mounted() {
+      /**
+       * iframe-宽高自适应显示
+       */
+      function changeMobsfIframe() {
+        const mobsf = document.getElementById('mobsf')
+        const deviceWidth = document.body.clientWidth
+        const deviceHeight = document.body.clientHeight
+        mobsf.style.width = Number(deviceWidth) - 220 + 'px' //数字是页面布局宽度差值
+        mobsf.style.height = Number(deviceHeight) - 80 + 'px' //数字是页面布局高度差
+      }
+
+      changeMobsfIframe()
+
+      window.onresize = function () {
+        changeMobsfIframe()
+      }
+    },
+  }
+</script>
