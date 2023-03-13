798a548facfcf36f59c7f7b4162c0d9ffffd7ebb
hjtahmakbar <hjtahmakbar@gmail.com>
2023-03-13 21:41:15 +0800

    feat: add SearchResult

--- src/views/dashboard/document-based-search/index.vue
+++ src/views/dashboard/document-based-search/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="p-4">
     <DocSearch class="!my-4 enter-y" :loading="loading" />
+    <div class="md:flex enter-y">
+      <EntitiesInvolved class="md:w-1/3 w-full" :loading="loading" />
+      <PeopleInvolved class="md:w-1/3 !md:mx-4 !md:my-0 !my-4 w-full" :loading="loading" />
+      <CompanyInvolved class="md:w-1/3 w-full" :loading="loading" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue'
   import DocSearch from './components/DocSearch.vue'
+  import EntitiesInvolved from './components/EntitiesInvolved.vue'
+  import PeopleInvolved from './components/PeopleInvolved.vue'
+  import CompanyInvolved from './components/CompanyInvolved.vue'
 
   const loading = ref(true)
 
