444abb7e0480b27234ef054ca09dea72755901f0
hjtahmakbar <hjtahmakbar@gmail.com>
2023-04-04 08:56:49 +0800

    fix: change some titles

--- src/locales/lang/en/sys.ts
+++ src/locales/lang/en/sys.ts
@@ -63,7 +63,7 @@ export default {
     signUpFormTitle: 'Sign up',
     forgetFormTitle: 'Reset password',
 
-    signInTitle: 'Backstage management system',
+    signInTitle: 'Sanction List Screening System Based on Named Entity Identification\n',
     signInDesc: 'Enter your personal details and get started!',
     policy: 'I agree to the xxx Privacy Policy',
     scanSign: `scanning the code to complete the login`,
