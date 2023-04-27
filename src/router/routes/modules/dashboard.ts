2525060f2aaf3e10343fceb0284ec6d8177e57fc
Jett Huang <hjtahmakbar@gmail.com>
2023-04-28 01:11:20 +0800

    feat: 优化系统

--- src/router/routes/modules/dashboard.ts
+++ src/router/routes/modules/dashboard.ts
@@ -7,30 +7,13 @@ const dashboard: AppRouteModule = {
   path: '/dashboard',
   name: 'Dashboard',
   component: LAYOUT,
-  redirect: '/dashboard/analysis',
+  redirect: '/dashboard/basicSearch',
   meta: {
     orderNo: 10,
     icon: 'ion:grid-outline',
-    title: t('routes.dashboard.dashboard'),
+    title: t('routes.dashboard.basicSearch'),
   },
   children: [
-    {
-      path: 'analysis',
-      name: 'Analysis',
-      component: () => import('/@/views/dashboard/analysis/index.vue'),
-      meta: {
-        // affix: true,
-        title: t('routes.dashboard.analysis'),
-      },
-    },
-    {
-      path: 'workbench',
-      name: 'Workbench',
-      component: () => import('/@/views/dashboard/workbench/index.vue'),
-      meta: {
-        title: t('routes.dashboard.workbench'),
-      },
-    },
     {
       path: 'basicSearch',
       name: 'BasicSearch',
